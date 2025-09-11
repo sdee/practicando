eeb3c131ca01c995e5fc4e44cd662e2e24d59776
Sutee Dee <sutee.dee@alumni.olin.edu>
2025-09-11 17:15:01 -0500

    Reconciles migration heads

---
+++ backend/alembic/versions/d74450cbe5c0_merge_heads_for_skipped_column.py
@@ -0,0 +1,28 @@
+"""merge heads for skipped column
+
+Revision ID: d74450cbe5c0
+Revises: add_skipped_to_guesses_20250911, 611307de519f
+Create Date: 2025-09-11 11:13:05.196636
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'd74450cbe5c0'
+down_revision: Union[str, Sequence[str], None] = ('add_skipped_to_guesses_20250911', '611307de519f')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
