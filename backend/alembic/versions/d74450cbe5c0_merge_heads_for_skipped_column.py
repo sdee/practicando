c3b30bdb247a55254e80c994331790225d823881
Sutee Dee <sutee.dee@alumni.olin.edu>
2025-09-18 09:40:55 -0500

    reconciles migrations

--- backend/alembic/versions/d74450cbe5c0_merge_heads_for_skipped_column.py
+++ backend/alembic/versions/d74450cbe5c0_merge_heads_for_skipped_column.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'd74450cbe5c0'
-down_revision: Union[str, Sequence[str], None] = ('add_skipped_to_guesses_20250911', '611307de519f')
+down_revision: Union[str, Sequence[str], None] = ('611307de519f')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
